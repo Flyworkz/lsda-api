b5aff15df4cb513bff0b0abb9d70363f00a999af
Flyworkz <pedron.dorian@gmail.com>
2020-10-05 11:03:34 +0200

    Structure Ok

--- app/server.js
+++ app/server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const router = require('./router');
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use('/v1', router);
 
 app.listen(port, () => {
     console.log(`Running on http://localhost:${port}`);
